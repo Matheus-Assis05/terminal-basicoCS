52ad3cc09f55de31a25d2cbeba3987f4d7e8b9fb
Matheus-Assis05 <matheusassisdev@gmail.com>
2023-09-27 14:06:45 -0300

    Algumas funcoes prontas, menu sobre OK, classe Menu OK

--- Program.cs
+++ Program.cs
@@ -4,28 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace terminal_basicoCS
+namespace terminal_basicoCS { 
 
     internal class Program
     {
         static void Main(string[] args)
         {
-        // Neste programa irei fazer a mesma coisa que fiz no java porem utilizando C# 
-        // Repositório de referência: https://github.com/Matheus-Assis05/Java-primeiro-projeto-terminal
-        // Apenas um teste para ver se a estrutura básica funciona no C#
+            // Neste programa irei fazer a mesma coisa que fiz no java porem utilizando C# 
+            // Repositório de referência: https://github.com/Matheus-Assis05/Java-primeiro-projeto-terminal
+            // Apenas um teste para ver se a estrutura básica funciona no C#
+            /* To Do:
+            * - Adicionar os comandos listados em help. 
+            * - Criar o submenu "sobre". 
+            * - Criar uma forma do programa funcionar como um terminal e um comando de quit (FEITO)
+            * - Criar a opçâo que permite o usuario consultar a data e hora 
+            */
 
-        while (true)
+            Console.WriteLine("***************************************************************************************\n" +
+                    "Desenvolvido por Matheus Martins usando Java 21. Obviamente Direitos nao Resevados\n" +
+                    "Terminal Simples em java - 23/09/2023\n" + 
+                    "***************************************************************************************\n\n" +
+                    "  digite 'help' para ver a lista de comandos\n"); 
+
+            //Chamando a calsse menuOpcoes criando um objeto menu
+            menuOpcoes menu = new menuOpcoes();
+
+            //while loop para reproduzir multiplos inputs do usuario
+            while (true)
             {
-                Console.Write("->$");
+                Console.Write("->$ ");
                 String input = Console.ReadLine();
 
-                if(input == null)
+                if(input == null) //caso nao haja input
                 {
                     Console.WriteLine("ERROR: No input");
-                }else if(input == "test") {
-                    Console.WriteLine("you just made a test");
-                }else if(input == "quit")
+                }else if(input == "help") {
+                    menu.cmdHelp(); //funcao cmdHelp() da classe menu
+                }else if(input == "sobre")
                 {
+                    menu.cmdSobre();
+                }
+                else if(input == "quit")
+                {
+                    Console.WriteLine("aperte ENTER para sair.");
                     break;
                 }                
             }
---
+++ menuOpcoes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace terminal_basicoCS
+{
+    internal class menuOpcoes
+    {
+        // Métodos da classe de menu que sao chamados no codigo principal
+        public void cmdHelp()
+        {
+            Console.WriteLine("Comandos: \n" +
+            "sobre - Sub-menu de descricao do produto \n" +
+            "versaojava - Repositorio no GitHub da versao java deste projeto\n" +
+            "hora - Mostra o dia e hora atual\n" +
+            "quit - Fecha o programa\n");
+        }
+
+        public void cmdSobre()
+        {
+            Console.WriteLine("Menu Sobre:\n Digite um comando -- nome; lang; desc; voltar");
+
+
+            //se repete o esquema do while infinito para o usuario realizar varios inputs
+            while(true)
+            {
+                Console.Write("-$> ");
+                String inputSobre = Console.ReadLine(); //Recebe na variavel inputSobre o input do usuario do menu sobre
+
+                if (inputSobre == "nome")
+                {
+                    Console.WriteLine("Desenvovido por: github.com/Matheus-assis05");
+                } else if (inputSobre == "lang")
+                {
+                    Console.WriteLine("C# .NET Console Application - Visual Studio 2022");
+                } else if (inputSobre == "desc")
+                {
+                    Console.WriteLine("Terminal simples com alguns comandos. A finalidade \n" +
+                    "do projeto é revisar e fixar a sintaxe e funcionamento C# e aprender sobre classes e importações");
+                } else if (inputSobre == "voltar")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("ERRO: COMANDO INVÁLIDO");
+                }
+
+            }
+        }
+    }
+}
